8e5a017f9955c4eb02eda9aa0ba1cded365456f0
The-Anonymousguy <tolubabatunde06@gmail.com>
2022-10-29 02:12:23 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function buttonclicked(){
+    alert('Click OK to continue');
+}
+
+function warning(){
+    let var1 = document.getElementById('targettag');
+    var1.innerHTML = "Javascript Text"
+}
